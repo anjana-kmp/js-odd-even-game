660319eb784ff9f26bd992f58d21bbbd75a6b733
anjana-kmp <71718802+anjana-kmp@users.noreply.github.com>
2020-11-04 20:57:52 +0530

    shuffling the boxes/'numbers in boxes'

--- oddevengame.js
+++ oddevengame.js
@@ -1,15 +1,44 @@
-function makeBoxes(){
-    let container = document.querySelector('.container');
+var arr=[]
+function shuffleNumbers(){
+    arr=[]
     let i=0
     while(i<100){
-        console.log(i+1,' is the number')
-        i++
-        let box = document.createElement('div')
-        box.className = 'box'
-        box.textContent = i
-        container.appendChild(box)
-
+        // console.log(i+1,' is the number')
+        i++ 
+        
+        if( i>=0){
+            // debugger
+            arr.push(i)
+        }
+        if (i>10){
+            let aIndex =Math.floor(Math.random()*arr.length)
+            let a = arr[aIndex]
+            let b = arr[i]
+            let c = arr[aIndex]
+            // a = b
+            // b = c
+            arr [aIndex] = b
+            arr [i] = c
+            
+        }
+        
     }
+    // console.log(arr)
+    createBoxes(arr)
+}
+function createBoxes(arr){
+    
+    let container = document.querySelector('.container');
+    arr.forEach(i => {
+        if(i>=0){
+            let box = document.createElement('div')
+            box.className = 'box'
+            box.textContent = i
+            container.appendChild(box)
+        }
+       
+    });
+    
 }
 function startTimer(){
     
@@ -20,4 +49,4 @@ function startGame(){
 function showScore(){
 
 }
-makeBoxes()
+shuffleNumbers()
